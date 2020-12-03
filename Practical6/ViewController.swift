c49549112349c44ede9e270888192c266392f064
shrys <srhyschua@gmail.com>
2020-12-03 09:14:44 +0800

    Initial Commit

---
+++ Practical6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical6
+//
+//  Created by Shane-Rhys Chua on 3/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
